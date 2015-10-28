466cbf4cdc7cdcce32134c473f2dc8df47cc4252
Radek <radek.baczynski@gmail.com>
2015-10-28 13:54:37 +0100

    formatting

--- src/templates/resetModified.php
+++ src/templates/resetModified.php
@@ -1,9 +1,9 @@
 
-	if (null !== $col && array_key_exists($col, $this->previousValues))
-	{
-		unset($this->previousValues[$col]);
-	}
-	else
-	{
-		$this->previousValues = [];
-	}
+		if (null !== $col && array_key_exists($col, $this->previousValues))
+		{
+			unset($this->previousValues[$col]);
+		}
+		else
+		{
+			$this->previousValues = [];
+		}
